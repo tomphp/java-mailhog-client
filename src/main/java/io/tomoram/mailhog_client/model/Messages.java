7d78e8051b616814538cd9a37a263a987f712a72
Tom Oram <tom@codept.com>
2017-10-13 20:37:45 +0100

    Enhancement: Add equality and to string methods Messages

--- src/main/java/io/tomoram/mailhog_client/model/Messages.java
+++ src/main/java/io/tomoram/mailhog_client/model/Messages.java
@@ -12,6 +12,28 @@ public final class Messages {
         this.messages = messages;
     }
 
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final Messages messages1 = (Messages) o;
+
+        return messages != null ? messages.equals(messages1.messages) : messages1.messages == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return messages != null ? messages.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Messages{" +
+                "messages=" + messages +
+                '}';
+    }
+
     public int getCount() {
         return count;
     }
--- src/test/java/io/tomoram/mailhog_client/api/v2/APIv2MessageListFetcherShould.java
+++ src/test/java/io/tomoram/mailhog_client/api/v2/APIv2MessageListFetcherShould.java
@@ -1,5 +1,35 @@
-import static org.junit.Assert.*;
+package io.tomoram.mailhog_client.api.v2;
+
+import io.tomoram.mailhog_client.HTTPClient;
+import io.tomoram.mailhog_client.exceptions.RequestFailed;
+import io.tomoram.mailhog_client.helpers.JSON;
+import io.tomoram.mailhog_client.model.Message;
+import io.tomoram.mailhog_client.model.Messages;
+import org.junit.Test;
+
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class APIv2MessageListFetcherShould {
 
+    private final HTTPClient http = mock(HTTPClient.class);
+    private final APIv2MessageListFetcher fetcher = new APIv2MessageListFetcher(http);
+
+    @Test
+    public void
+    parse_a_list_of_messages() throws RequestFailed {
+        when(http.get("/messages")).thenReturn(JSON.messageCollection(JSON.singleMessage()));
+
+        final Message message = Message.builder()
+                .addRecipient("george@a-happy-place.com")
+                .setSender("mental@rhino.com")
+                .build();
+
+        Messages expected = new Messages(1, Collections.singletonList(message));
+
+        assertThat(fetcher.fetchFrom("/messages")).isEqualTo(expected);
+    }
 }
